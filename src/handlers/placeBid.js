78842c1a8cfac5d4fda64bf684ec81337839bb65
nagaralu <rnsm25@gmail.com>
2020-11-19 15:45:31 -0600

    new lambda function to place a bid using DynamoDB update

---
+++ src/handlers/placeBid.js
@@ -0,0 +1,40 @@
+import AWS from 'aws-sdk';
+import commonMiddleware from '../lib/commonMiddleware';
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+
+  // DynamoDB Update:
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#update-property
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id }, //update the auction that has this id using Key
+    // highest bid object with amount property will be set with this bid
+    UpdateExpression: 'set highestBid.amount = :amount',
+    ExpressionAttributeValues: {
+      ':amount': amount
+    },
+    ReturnValues: 'ALL_NEW'
+  };
+
+  let updatedAuction;
+
+  try {
+    const result = await dynamodb.update(params).promise();
+    updatedAuction = result.Attributes;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction)
+  };
+}
+
+export const handler = commonMiddleware(placeBid);
