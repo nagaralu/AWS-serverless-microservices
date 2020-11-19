eacc0ef4d00af945fcd1abf99494850163d84582
nagaralu <rnsm25@gmail.com>
2020-11-18 20:07:57 -0600

    DynamoDB table created in yaml file

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,34 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   // event: this object includes all the info about createAuction event - i.e event body, path params, query params, headers etc.
   // context: this includes meta data, custom data can be added to both event and context but context is preferred
   // context can be used as middleware, like authentication
+
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    id: uuid(),
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString()
+  };
+
+  // insert the new item into DynamoDB table using put (create a new item)
+  await dynamodb
+    .put({
+      TableName: 'AuctionsTable', //note do not use camel case, use AWS document syntax
+      Item: auction
+    })
+    .promise();
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context })
+    statusCode: 201,
+    body: JSON.stringify(auction)
   };
 }
 
