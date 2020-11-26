4b2790b8e84f2768981b0fc2a818b547bc749f93
nagaralu <rnsm25@gmail.com>
2020-11-26 00:25:03 -0600

    cors added to lambdas and commonmiddleware

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -6,6 +6,7 @@ import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
 import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
 import uploadAuctionPictureSchema from '../lib/schemas/uploadAuctionPictureSchema';
+import cors from '@middy/http-cors';
 
 export async function uploadAuctionPicture(event) {
   const { id } = event.pathParameters;
@@ -38,4 +39,5 @@ export async function uploadAuctionPicture(event) {
 
 export const handler = middy(uploadAuctionPicture)
   .use(httpErrorHandler())
-  .use(validator({ inputSchema: uploadAuctionPictureSchema }));
+  .use(validator({ inputSchema: uploadAuctionPictureSchema }))
+  .use(cors());
